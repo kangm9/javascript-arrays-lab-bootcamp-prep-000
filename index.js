6450cfb6489bb6cf0c714ebae1cbe13c79b3f4d6
Min Kang <kangm914@gmail.com>
2019-04-25 13:39:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function destructivelyAppendKitten(name) {
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.
+  return kittens.unshift(name);
 } 
+
+
 describe('destructivelyPrependKitten(name)', function() {
     it('prepends a kitten to the beginning of the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
