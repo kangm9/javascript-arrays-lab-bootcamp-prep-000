13013df4189a09e9299993725dcb182169200612
Min Kang <kangm914@gmail.com>
2019-04-25 13:26:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"] 
 
-function destructivelyAppendKitten(array, name) {
-  return [array, ...name];
+function destructivelyAppendKitten(arrays, name) {
+  return [arrays, ...name];
 }
