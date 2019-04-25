166ae113892f17222b86cd20b12d52eb1665a2c4
Min Kang <kangm914@gmail.com>
2019-04-25 13:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] 
 
 function destructivelyAppendKitten(array, name) {
-  return [...name]
+  return [array...name]
 describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
