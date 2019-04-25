64b3ea5ae4c5443c2300f49425d2af5c34e6fa27
Min Kang <kangm914@gmail.com>
2019-04-25 13:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var kittens = ["Milo", "Otis", "Garfield"] 
 
+function destructivelyAppendKitten
+describe('destructivelyAppendKitten(name)', function() {
+    it('appends a kitten to the end of the kittens array', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      destructivelyAppendKitten('Ralph')
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
+      
+      
